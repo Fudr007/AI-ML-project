702a162d334229bc5b73119698f47b947811fd4a
Fudr007 <petr.valenta00@email.cz>
2026-04-18 16:09:13 +0200

    Web app

---
+++ App/static/script.js
@@ -0,0 +1,91 @@
+function fetchRoster() {
+    const url = document.getElementById('sofascore_url').value;
+    const statusLabel = document.getElementById('fetch_status');
+
+    if(!url) {
+        statusLabel.innerText = "Put URL to the field!";
+        statusLabel.style.color = "red";
+        return;
+    }
+
+    statusLabel.innerText = "Dowloading from Sofascore API...";
+    statusLabel.style.color = "blue";
+
+    fetch('/fetch_roster', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.error) {
+            statusLabel.innerText = data.error;
+            statusLabel.style.color = "red";
+        } else {
+            document.querySelector('input[name="team_home"]').value = data.home_team;
+            document.querySelector('input[name="team_away"]').value = data.away_team;
+
+            document.querySelector('input[name="date"]').value = new Date(data.date * 1000).toISOString().split('T')[0];
+
+
+            function fillSection(playerArray, containerId) {
+                const container = document.getElementById(containerId);
+                let inputs = container.querySelectorAll('input');
+
+                while (inputs.length < playerArray.length) {
+                    addField(containerId);
+                    inputs = container.querySelectorAll('input');
+                }
+
+                playerArray.forEach((player, index) => {
+                    inputs[index].value = player.name;
+                });
+            }
+
+            const homeGKs = data.home_players.filter(p => p.position === 'G');
+            const homePLs = data.home_players.filter(p => p.position !== 'G');
+
+            fillSection(homeGKs, 'gk_home');
+            fillSection(homePLs, 'pl_home');
+
+            const awayGKs = data.away_players.filter(p => p.position === 'G');
+            const awayPLs = data.away_players.filter(p => p.position !== 'G');
+
+            fillSection(awayGKs, 'gk_away');
+            fillSection(awayPLs, 'pl_away');
+
+            statusLabel.innerText = "Roster downloaded and added successfully!";
+            statusLabel.style.color = "green";
+        }
+    })
+    .catch(error => {
+        statusLabel.innerText = "Critical error: " + error;
+        statusLabel.style.color = "red";
+    });
+}
+
+function addField(type){
+    const teamSuffix = type.includes('home') ? 'home' : 'away';
+
+    const gkCount = document.querySelectorAll('#gk_' + teamSuffix + ' input').length;
+    const plCount = document.querySelectorAll('#pl_' + teamSuffix + ' input').length;
+    const totalPlayers = gkCount + plCount;
+
+    if (totalPlayers >= 16) {
+        alert("Roster is full! Highest number of players is 16.");
+        return;
+    }
+    const field = document.createElement('input');
+    field.required = true;
+    field.type = "text";
+    if (type.includes('gk')){
+        field.name = type;
+        field.placeholder = "Goalkeeper name"
+    }else {
+        field.name = type;
+        field.placeholder = "Player name"
+    }
+    document.getElementById(type).appendChild(field);
+}
